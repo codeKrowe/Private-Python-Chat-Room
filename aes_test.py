b52d2501f782c3f515a5aed7e145a0a9b5f6e9d2
cathaldcronin1 <10131531@studentmail.ul.ie>
2015-02-05 22:58:00 +0000

    AES Encrpytion

---
+++ aes_test.py
@@ -0,0 +1,26 @@
+from AES_Class import *
+
+print "Starting test 128\n"
+
+a = AESClass("cbc",128,0,"hex")
+a.setupAES()
+testenc = a.enc_str("Greetings and Salutations!!")
+
+print "Testing Decyption"
+testdec = a.dec_str(testenc)
+
+
+print "\n256"
+b = AESClass("cbc",256,0,"hex")
+b.setupAES()
+testenc = b.enc_str("Greetings and Salutations!!")
+
+print "Testing Decyption"
+testdec = b.dec_str(testenc)
+
+
+print "testing compression"
+b.compress_zip("test.py","test.zip")
+
+
+#b.decompress_zip("test.zip","/Users/jonathan/Desktop/new")
