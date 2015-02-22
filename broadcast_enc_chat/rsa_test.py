afcd3e6412b21cea58568b61d7d13afad323f573
cathaldcronin1 <10131531@studentmail.ul.ie>
2015-02-22 15:23:54 +0000

    minor change to test file

--- broadcast_enc_chat/rsa_test.py
+++ broadcast_enc_chat/rsa_test.py
@@ -4,27 +4,27 @@ def main():
 
   # Create our rsa encryption class
   rsa = RSAClass()
-  
+
   # some plaintext we want to encrypt
-  plainText = "Gonna encrypt the shit out of this string!"
+  plainText = "Gonna encrypt this string!"
 
   # Generate key pair
   public_key, private_key = rsa.generate_keys()
 
-  # Encrpyt our string
+  # Encrypt our string
   cipherText = rsa.encrypt_text(plainText, public_key)
 
   # decrypt our string
   plainText = rsa.decrypt_text(cipherText, private_key)
 
-  print "using pub for encryption"
+  print "using public_key for encryption"
   print cipherText
   print plainText
 
-
+  # Encrypt with private to sign message.
   cipherText = rsa.encrypt_with_private(plainText, private_key)
 
-  # decrypt our string
+  # decrypt our message with corresponding public key
   plainText = rsa.decrypt_with_public(cipherText, public_key)
 
   print "using private_key for encryption"
