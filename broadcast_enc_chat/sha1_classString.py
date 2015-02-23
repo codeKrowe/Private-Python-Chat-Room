87bbb3a19a6e45f95ec8996cbced357323de72a5
dreadeye <nidhu12345@gmail.com>
2015-02-23 10:34:01 +0000

    updated

--- broadcast_enc_chat/sha1_classString.py
+++ broadcast_enc_chat/sha1_classString.py
@@ -1,8 +1,9 @@
 #class implementaion for hash(sha-1) of a string
 # ref-chilkat-http://www.example-code.com/python/crypt_hash_algorithms.asp 
-
+# ref- pytho docs https://docs.python.org/2/library/sha.html
 import sys
 import chilkat
+import base64
 crypt = chilkat.CkCrypt2()
 success = crypt.UnlockComponent("T12302015Crypt_sHyDCAFgIR1v")
 
@@ -17,13 +18,13 @@ class hash_sha1:
 	def __init__(self,passString):
 		self.h_string=passString
 
-	# method to create a hash
+	# method to create a hash-sha-1
 	def h_sha1(self):
 		global crypt
 		crypt.put_HashAlgorithm("sha1")
 		crypt.put_EncodingMode("hex")
 		self.hash = crypt.hashStringENC(self.h_string)
-		print self.hash
+		#print self.hash
 
 	#method to display the created hash	
 	def dispHash(self):
--- broadcast_enc_chat/sha1_test_string.py
+++ broadcast_enc_chat/sha1_test_string.py
@@ -1,4 +1,9 @@
 #hashing of string (sha-1)
+#-160 bit output
+
+import sys
+import binascii
+import hashlib
 
 from sha1_classString import *
 
@@ -7,17 +12,17 @@ from sha1_classString import *
 get_String=raw_input("enter the string :")
 hash_1=hash_sha1(get_String)
 hash_1.h_sha1()
-hash_1.dispHash()
+print "the hash for the entered string: ",hash_1.dispHash()
 
 #second hash
 #enter the string to be hashed 
 get_String2=raw_input("enter the string :")
-hash_3=hash_sha1(get_String2)
-hash_3.h_sha1()
-hash_3.dispHash()
+hash_2=hash_sha1(get_String2)
+hash_2.h_sha1()
+print "the hash for the entered string: ",hash_2.dispHash()
 
 #comparison of the two hash
-if hash_1.dispHash() == hash_3.dispHash():
+if hash_1.dispHash() == hash_2.dispHash():
 	print "same string"
 else:
 	print "string modified"
